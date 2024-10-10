6a5fb826cc0848eedece60476d4e8f1a9a457415
TylexTX <dseityahya@gmail.com>
2024-10-10 20:14:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+let player = true;
+let playing = true;
+const squares = {
+    lt:"",
+    mt:"",
+    rt:"",
+    lm:"",
+    mm:"",
+    rm:"",
+    lb:"",
+    mb:"",
+    rb:""
+}
+
+function restart() {
+    const squaresID = document.querySelectorAll('td');
+    squaresID.forEach(function(square) {
+        square.innerText = "";
+    });
+
+    for (let i in squares) {
+        squares[i] = "";
+    }
+    
+    document.getElementById("output").innerText = "It's X's turn";
+    playing = true;
+    player = true;
+}
+
+function checkRow(one, two, three) {
+    if (one == two && two == three) {
+        document.getElementById("output").innerText = one + " won the game";
+        playing = false;
+    }
+}
+
+function click(event) {
+    if (playing) {
+        const id = event.target.id;
+        if (squares[id] == "") {
+            if (player) {
+                document.getElementById(id).innerText = "X";
+                document.getElementById(id).style.color = "#dd2222";
+                document.getElementById("output").innerText = "It's O's turn";
+                squares[id] = "X";
+                player = false;
+            } else {
+                document.getElementById(id).innerText = "O";
+                document.getElementById(id).style.color = "#2222dd";
+                document.getElementById("output").innerText = "It's X's turn";
+                squares[id] = "O";
+                player = true;
+            }
+        }
+
+        if (squares.lt != "") {
+            checkRow(squares.lt, squares.mt, squares.rt);
+            checkRow(squares.lt, squares.lm, squares.lb);
+        }
+        if (squares.mm != "") {
+            checkRow(squares.lt, squares.mm, squares.rb);
+            checkRow(squares.lm, squares.mm, squares.rm);
+            checkRow(squares.lb, squares.mm, squares.rt);
+            checkRow(squares.mb, squares.mm, squares.mt);
+        }
+        if (squares.rb != "") {
+            checkRow(squares.rb, squares.rm, squares.rt);
+            checkRow(squares.rb, squares.mb, squares.lb);
+        }
+    }
+}
+
+const squaresID = document.querySelectorAll('td');
+squaresID.forEach(function(square) {
+    square.addEventListener('click', click);
+});
